bd19b301df7d8f32f15b4f78c8da580d748cd980
Tony Hoyle <tony@hoyle.me.uk>
2017-08-19 23:50:17 +0100

    Ditch storyboards, do it programmatically as it's about 10 times easier and doesn't involve trying to interact with the abomination that is xcode

---
+++ EH/iOS/MainScreen.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UIKit;
+
+namespace ClockworkHighway.iOS
+{
+    public class MainScreen : UITabBarController
+    {
+        UIViewController chargers, settings;
+        bool loggedIn;
+
+        public MainScreen()
+        {
+			loggedIn = false;
+
+			chargers = new UIViewController();
+            chargers.Title = "Charge";
+
+			settings = new UIViewController();
+            settings.TabBarItem = new UITabBarItem(UITabBarSystemItem.More, 0);
+
+            var tabs = new UIViewController[] { chargers, settings };
+            ViewControllers = tabs;
+        }
+
+        public override void ViewDidAppear(bool animated)
+        {
+            base.ViewDidAppear(animated);
+
+            if(!loggedIn)
+                ShowLogin();
+        }
+
+		private void ShowLogin()
+		{
+			var alert = UIAlertController.Create("Login", "Enter your credentials", UIAlertControllerStyle.Alert);
+			var login = UIAlertAction.Create("Login", UIAlertActionStyle.Default, (Action) => { });
+
+			alert.AddAction(login);
+
+			alert.AddTextField((obj) => { obj.Placeholder = "Username"; });
+			alert.AddTextField((obj) => { obj.Placeholder = "Password"; obj.SecureTextEntry = true; });
+
+			PresentViewController(alert, true, null);
+		}
+	}
+}
